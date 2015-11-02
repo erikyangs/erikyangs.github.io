139e8631175ba2fb78aca5e53be3580d37303dc4
Erik Yang <erikyangs@gmail.com>
2015-11-02 00:34:06 -0800

    overhauled content on bottom, added links to actual things, made animated text with javascript, BIG UPDATE WOO

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ $(document).ready(function() {
 	    var section = $(this).attr("href");
 	    $("html, body").animate({
 	        scrollTop: $(section).offset().top
-	    });
+	    }, 1500, "easeOutExpo");
 	});
+
+	var t = ["CODE", "INNOVATE", "DESIGN", "HACK", "COLLABORATE", "FILM", "ADVENTURE"];
+    var counter = 0;
+    change();
+    setInterval(change, 1000);
+    function change() {
+		$("#todesign").text(t[counter]);
+        counter++;
+        if(counter >= t.length){
+        	counter = 0;
+        }
+    }
+
 });
