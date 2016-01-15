8a3f78c5771154d843e23fa48285da6b5659f973
Erik Yang <erikyangs@gmail.com>
2016-01-15 00:47:54 -0800

    troubleshooting

--- js/angularapp.js
+++ js/angularapp.js
@@ -1,14 +1,14 @@
 console.log("lol");
 
 var app = angular.module('app',['$ngRoute']);
-
+/*
 app.config(function($routeProvider){
 	$routeProvider
 	.when('/',{
 		templateUrl:'views/home.html',
 		controller: 'mainController'
 	})
-});
+});*/
 
 app.controller('mainController', [function(){
 
