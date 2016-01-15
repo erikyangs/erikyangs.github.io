9cd9bfffc164228f1bdcefc2dd9e52b3a9010020
Erik Yang <erikyangs@gmail.com>
2016-01-15 01:03:45 -0800

    additional angular troubleshooting

--- js/angularapp.js
+++ js/angularapp.js
@@ -1,15 +1,17 @@
-console.log("lol");
 
-var app = angular.module('app',['$ngRoute']);
-/*
+var app = angular.module('app',['ngRoute']);
+
 app.config(function($routeProvider){
 	$routeProvider
+
 	.when('/',{
 		templateUrl:'views/home.html',
 		controller: 'mainController'
 	})
-});*/
+	.otherwise({ 
+		redirectTo: '/'
+	});
+});
 
 app.controller('mainController', [function(){
-
 }]);
