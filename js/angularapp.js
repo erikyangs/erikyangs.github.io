b9f938435adbb5eb997f639b8faac3389657abb6
Erik Yang <erikyangs@gmail.com>
2016-01-15 00:34:56 -0800

    angularJS

---
+++ js/angularapp.js
@@ -0,0 +1,15 @@
+console.log("lol");
+
+var app = angular.module('app',['$ngRoute']);
+
+app.config(function("$routeProvider"){
+	$routeProvider
+	.when('/',{
+		templateUrl:'views/home.html',
+		controller: 'mainController'
+	})
+});
+
+app.controller('mainController', [function(){
+
+}]);
