f9cce2ccf58a76ef1290522b9d1df161003181df
Erik Yang <erikyangs@gmail.com>
2016-02-07 22:10:04 -0800

    blips on click

--- js/404.js
+++ js/404.js
@@ -1,3 +1,7 @@
+var numOfShines=1;
 $(document).on("click", function(e) {
-	$("body").append("<img src='/img/shine.png' alt='blip' class='shine'></img>");
+	$("body").append("<img src='/img/shine.png' alt='blip' class='shine' id='shine" + numOfShines +"''></img>");
+	$("#shine"+numOfShines).css("top",e.pageY);
+	$("#shine"+numOfShines).css("left",e.pageX);
+	numOfShines++;
 });
