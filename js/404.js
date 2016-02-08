d1dddaf430438d38737458120deb1d010ef9fadc
Erik Yang <erikyangs@gmail.com>
2016-02-07 22:30:44 -0800

    sounds of blipping

--- js/404.js
+++ js/404.js
@@ -1,7 +1,28 @@
+//send me to the depths of the shadow realm for doing this
 var numOfShines=1;
 $(document).on("click", function(e) {
 	$("body").append("<img src='/img/shine.png' alt='blip' class='shine' id='shine" + numOfShines +"''></img>");
-	$("#shine"+numOfShines).css("top",e.pageY);
-	$("#shine"+numOfShines).css("left",e.pageX);
+	$("#shine"+numOfShines).css("top",e.pageY-25);
+	$("#shine"+numOfShines).css("left",e.pageX-25);
 	numOfShines++;
+});
+
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'audio.mp3');
+    //audioElement.setAttribute('autoplay', 'autoplay');
+    //audioElement.load()
+
+    $.get();
+
+    audioElement.addEventListener("load", function() {
+        audioElement.play();
+    }, true);
+
+    $('body').click(function() {
+        audioElement.play();
+        setTimeout(function(){
+        	audioElement.pause();
+        }, 500);
+    });
 });
