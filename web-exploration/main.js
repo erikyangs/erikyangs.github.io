3a976866f95979e1f348b0881206b5f01528d3b0
Erik Yang <erikyangs@gmail.com>
2016-04-14 20:22:43 -0700

    changed web-ex

--- web-exploration/main.js
+++ web-exploration/main.js
@@ -1,15 +1,10 @@
-var x = 0;
-window.setInterval(function(){
-	if(x==0){
-		$(".container").css("background-color", "yellow");
-		$(".center").css("background-color", "black");
-		$(".center").css("color", "yellow");
-		x++;
-	}
-	else{
-		$(".container").css("background-color", "black");
-		$(".center").css("background-color", "yellow");
-		$(".center").css("color", "black");
-		x--;
-	}
-}, 10);
+$(document).ready(function(){
+	$(function(){
+      $("p").typed({
+        strings: ["stealing this plugin.", "dank memes.", "Birdie Sanders 2016."],
+        contentType: 'html',
+        showCursor: true,
+        cursorChar: "|memes"
+      });
+  	});
+});
