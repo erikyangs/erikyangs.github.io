da6aed7f79c672dfc661ccd312feb62528b85456
Erik Yang <erikyangs@gmail.com>
2017-02-07 19:41:36 -0800

    updating 404 shine image

--- archive/2016-05/js/404.js
+++ archive/2016-05/js/404.js
@@ -1,7 +1,7 @@
 //send me to the depths of the shadow realm for doing this
 var numOfShines=1;
 $(document).on("click", function(e) {
-	$("body").append("<img src='/img/shine.png' alt='blip' class='shine' id='shine" + numOfShines +"''></img>");
+	$("body").append("<img src='https://erikyangs.github.io/archive/2016-05/img/shine.png' alt='blip' class='shine' id='shine" + numOfShines +"''></img>");
 	$("#shine"+numOfShines).css("top",e.pageY-25);
 	$("#shine"+numOfShines).css("left",e.pageX-25);
 	numOfShines++;
@@ -9,7 +9,7 @@ $(document).on("click", function(e) {
 
 $(document).ready(function() {
     var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'shine.wav');
+    audioElement.setAttribute('src', 'https://erikyangs.github.io/archive/2016-05/shine.wav');
     //audioElement.setAttribute('autoplay', 'autoplay');
     //audioElement.load()
 	
